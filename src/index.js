33d0e6136d022335d22a6ba137aee2173fad69e2
Victoria Le <leqvictoria@gmail.com>
2023-01-25 15:35:44 -0500

    clean up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { Container } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Container>
       <App />
-    </Container>
   </React.StrictMode>
 );
 
