ffef0414fc8400869356239152c2d0e372b1e2ce
Victoria Le <leqvictoria@gmail.com>
2023-01-26 17:21:17 -0500

    select item styled

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,42 @@
-import { AppBar, Container, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { AppBar, Container, createTheme, MenuItem, Select, styled, ThemeProvider, Toolbar, Typography } from '@mui/material'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-
+const StyledTypography = styled(Typography)({
+  flex: 1,
+  color: "gold",
+  fontWeight: "bold",
+  cursor: "pointer"
+})
 
 const Header = () => {
 
   const navigate = useNavigate()
 
-  const [open, setOpen] = useState(false)
-
-
+  const darkTheme = createTheme({
+    palette: {
+      primary: {
+        main: "#fff",
+      },
+      mode: "dark"
+    }
+  })
+  
   return (
+    <ThemeProvider theme={darkTheme}>
       <AppBar color='transparent' position='static'>
         <Container>
           <Toolbar>
-            <Typography> Coin Wise </Typography>
+            <StyledTypography onClick={() => navigate("/")} variant="h6"> Coin Wise </StyledTypography>
+
+            <Select variant='outlined' style={{width:100, height:40, marginRight: 15}}>
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"VND"}>VND</MenuItem>
+            </Select>
           </Toolbar>
         </Container>
       </AppBar>
+    </ThemeProvider>
   )
 }
 
