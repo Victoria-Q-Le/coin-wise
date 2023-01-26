6ac4c5603b3f3b73ef69b459e6dcb9253a552f81
Victoria Le <leqvictoria@gmail.com>
2023-01-26 12:59:30 -0500

    routes set up

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,20 @@
-import { Box } from '@mui/material'
+import { Home } from '@mui/icons-material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
 import React from 'react'
 
 const Sidebar = () => {
   return (
-    <Box bgcolor="lightcoral" flex={1} p={2} sx={{display: {xs : "none", sm: "block"}}}>
-      Side Bar 
+    <Box flex={1} p={2} sx={{display: {xs : "none", sm: "block"}}}>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href='/'>
+            <ListItemIcon>
+                <Home />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   )
 }
