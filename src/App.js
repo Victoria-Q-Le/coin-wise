4c57bd13c6245e7d1d5a226e0d0ad40761e76a45
Victoria Le <leqvictoria@gmail.com>
2023-01-26 16:08:54 -0500

    header styles

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import Header from "./components/Header";
 import CoinList from "./pages/CoinList";
 import CoinPage from "./pages/CoinPage";
 import Home from "./pages/Home";
+import './App.css'
+import { styled } from "@mui/material";
+
+const StyledContainer = styled("div")({
+  backgroundColor: "#14161a",
+  color: "white",
+  minHeight: "100vh"
+})
 
 function App() {
   return (
-    <div>
+    <StyledContainer>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/coins" element={<CoinList />} />
         <Route path="/coins/:id" element={<CoinPage />} />
       </Routes>
-    </div>
+    </StyledContainer>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import { AppBar, Container, Menu, MenuItem, styled, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+
 
 const Header = () => {
+
+  const navigate = useNavigate()
+
+  const [open, setOpen] = useState(false)
+
+
   return (
-    <div>
-      This is the Header component
-    </div>
+      <AppBar color='transparent' position='static'>
+        <Container>
+          <Toolbar>
+            <Typography> Coin Wise </Typography>
+          </Toolbar>
+        </Container>
+      </AppBar>
   )
 }
 
