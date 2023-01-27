3873d2844c1c28e382efb32997e15d18b28a7f8a
Victoria Le <leqvictoria@gmail.com>
2023-01-26 21:58:31 -0500

    carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,6 +1,8 @@
 import { styled } from '@mui/material'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import AliceCarousel from 'react-alice-carousel'
+import { Link } from 'react-router-dom'
 import {CoinState} from '../CoinContext'
 import {TrendingCoins} from '../config/api'
 
@@ -26,9 +28,38 @@ const Carousel = () => {
         fetchTrendingCoins()
     },[currency])
 
+    const items = trending.map((coin) => {
+
+        return(
+            <Link to={`/coins/${coin.id}`}>
+                <img src={coin?.image} alt={coin.name} height ='80' style={{marginBottom: 10}} />
+                <span> {coin.symbol} &nbsp <span></span> </span>
+            </Link>
+        )
+    })
+
+    const responsive ={
+        0: {
+            items: 2
+        },
+        512: {
+            items: 4
+        }
+    }
+
     return (
         <CarouselDiv>
-            Carousel
+            <AliceCarousel 
+                mouseTracking 
+                infinite 
+                autoPlay ={1000}
+                animationDuration={1500}
+                disableDotsControls
+                disableButtonsControls
+                responsive={responsive} //how many items this will display
+                autoPlay
+                items={items}
+            />
         </CarouselDiv>
     )
 }
