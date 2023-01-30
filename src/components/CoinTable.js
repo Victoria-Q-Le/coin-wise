07dd57cdb82e3a5178c7f3ca8da715aff9f0a8a7
Victoria Le <leqvictoria@gmail.com>
2023-01-30 17:56:36 -0500

    fetching coins table

--- src/components/CoinTable.js
+++ src/components/CoinTable.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import {CoinList} from '../config/api'
+import { CoinState } from '../CoinContext'
 
 const CoinTable = () => {
+
+  const [coins, setCoins] = useState([])
+  const [loading, setLoading] = useState(false)
+  
+  const {currency} = CoinState()
+
+
+  console.log(coins);
+
+  useEffect(() => {
+    const fetchCoins = async () => {
+      setLoading(true)
+      const {data} = await axios.get(CoinList(currency))
+      setCoins(data)
+      setLoading(false)
+    }
+    fetchCoins()
+  }, [currency])
+
   return (
     <div>
       This is the coin table 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import CoinTable from '../components/CoinTable'
 
 const Home = () => {
   return (
     <>
       <Banner />
+      <CoinTable />
     </>
   )
 }
