66427f431818d29827b578bd92060b413e432d25
Victoria Le <leqvictoria@gmail.com>
2023-01-31 18:43:03 -0500

    coin page responsive

---
+++ src/components/CoinInfo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CoinInfo = () => {
+  return (
+    <div>
+      This is the coin info component
+    </div>
+  )
+}
+
+export default CoinInfo
--- src/pages/CoinPage.js
+++ src/pages/CoinPage.js
@@ -1,15 +1,54 @@
-import React from 'react'
+import { styled } from '@mui/material'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import {CoinState} from '../CoinContext'
+import CoinInfo from '../components/CoinInfo'
+import { SingleCoin } from '../config/api'
 
+
+const DivContainer = styled("div")(({theme}) => ({
+    display: "flex",
+    [theme.breakpoints.down("md")]: {
+        flexDirection: "column",
+        alignItems: "center"
+    }
+}))
+
+const DivSideBar = styled("div")({
+    
+})
 
 
 const CoinPage = () => {
     const {id} = useParams() 
     
+    const [coin, setCoin] = useState()
+    
+    const {currency, symbol} = CoinState()
+
+    useEffect(() => {
+        const fetchCoin = async () => {
+            const {data} = await axios.get(SingleCoin(id))
+            setCoin(data)
+        }
+        fetchCoin()
+    },[id])
+
+    console.log(coin);
+
+    console.log(currency, symbol)
+     
+
     return (
-        <div>
-            This is the {id} coin page
-        </div>
+        <DivContainer>
+            <DivSideBar>
+                This is the side bar
+            </DivSideBar>
+            {/* END OF SIDE BAR */}
+
+            <CoinInfo coin={coin}/>
+        </DivContainer>
     )
 }
 
